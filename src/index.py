5ef1e39ec866419cc74b17ec7030a6bb08f97690
Miguel Daza <madazabran@unicesar.edu.co>
2022-11-03 18:14:36 -0500

    commit

--- src/index.py
+++ src/index.py
@@ -6,38 +6,16 @@ import pandas as pd
 from dash.dependencies import Input,Output
 
 df = pd.read_csv('data/captador_tweets.csv')
-
-#print(df)
-#print(df.vacuna_nombre.nunique())
-#print(df.vacuna_nombre.unique())
-
-app = dash.Dash(__name__)
+app = dash.Dash('Dvit')
 
 app.layout = html.Div([
     
-    html.Div([
+    html.Header([
         html.H1('Lenguajes de programación'),
         html.Img(src='image/logo.png')
     ], className = 'banner'),
 
-    html.Div([
-        html.Div([
-            html.P('Selecciona el lenguaje', className = 'fix_label', style={'color':'black', 'margin-top': '2px'}),
-            dcc.RadioItems(id = 'lenguajes-radioitems', 
-                            labelStyle = {'display': 'inline-block'},
-                            options = [
-                                {'label' : 'Usuario', 'value' : 'Id'},
-                                {'label' : 'Texto', 'value' : 'Texto'}
-                                
-                            ]
-                        )
-        ])
-    ])
-
-   
-
 ],)
-
 
 if __name__ == ('__main__'):
     app.run_server(debug=True)
