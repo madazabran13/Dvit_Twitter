ea119fc624fec3bc444b3e4105c5f104f1d054d6
Miguel Daza <madazabran@unicesar.edu.co>
2022-11-03 21:33:18 -0500

    commit

---
+++ src/index.py
@@ -0,0 +1,51 @@
+import dash_bootstrap_components as dbc
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Output, Input
+import twitter  # pip install python-twitter
+from app import app, api
+from app import  api
+from mentions import mentions_layout
+
+
+
+# our app's Tabs *********************************************************
+app_tabs = html.Div(
+    [
+        dbc.Tabs(
+            [
+                dbc.Tab(label="Menciones", tab_id="tab-mentions", labelClassName="text-success font-weight-bold", activeLabelClassName="text-danger"),
+            ],
+            id="tabs",
+            active_tab="tab-mentions",
+        ),
+    ], className="mt-3"
+)
+
+app.layout = dbc.Container([ 
+    html.Hr(),
+    dbc.Row(dbc.Col(html.H1("Dvit: Analisis de resultados en Twiter",
+                            style={"textAlign": "center"}), width=12),
+            ),
+    html.Div([
+        html.Img(src='assets/vacuna.png')
+    ], className = 'banner'),
+    html.Hr(),
+    dbc.Row(dbc.Col(app_tabs, width=12), className="mb-3"),
+    html.Div(id='content', children=[])
+
+])
+
+@app.callback(
+    Output("content", "children"),
+    [Input("tabs", "active_tab")]
+)
+
+def switch_tab(tab_chosen):
+    if tab_chosen == "tab-mentions":
+        return mentions_layout
+    return html.P("Esto no debería mostrarse por ahora....")
+
+if __name__=='__main__':
+    app.run_server(debug=True)
---
+++ src/mentions.py
@@ -0,0 +1,132 @@
+import plotly.express as px
+import dash_bootstrap_components as dbc
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Output, Input, State
+import pandas as pd
+import twitter  # pip install python-twitter
+from app import app, api
+
+
+mentions_layout = html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Label("Número de resultados"),
+                        dcc.Dropdown(
+                            id="count-mentions",
+                            multi=False,
+                            value=50,
+                            options=[
+                                {"label": "50", "value": 50},
+                                {"label": "100", "value": 100},
+                                {"label": "200", "value": 200},
+                            ],
+                            clearable=False,
+                        ),
+                    ],
+                    width=3,
+                ),
+                dbc.Col(
+                    [
+                        html.Label("Busqueda"),
+                        dcc.Input(
+                            id="input-handle",
+                            type="text",
+                            placeholder="Mensaje a buscar",
+                            value="python",
+                        ),
+                    ],
+                    width=3,
+                ),
+            ],
+            className="mt-4",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Button(
+                            id="hit-button",
+                            children="Buscar",
+                            style={"background-color": "grey", "color": "white"},
+                        )
+                    ],
+                    width=2,
+                )
+            ],
+            className="mt-2",
+        ),
+        dbc.Row(
+            [
+                dbc.Col([dcc.Graph(id="myscatter", figure={})], width=6),
+                dbc.Col([dcc.Graph(id="myscatter2", figure={})], width=6),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.P(
+                            id="notification",
+                            children="",
+                            style={"textAlign": "center"},
+                        )
+                    ],
+                    width=12,
+                )
+            ]
+        ),
+    ]
+)
+
+
+# pull data from twitter and create the figures
+@app.callback(
+    Output(component_id="myscatter", component_property="figure"),
+    Output(component_id="myscatter2", component_property="figure"),
+    Output(component_id="notification", component_property="children"),
+    Input(component_id="hit-button", component_property="n_clicks"),
+    State(component_id="count-mentions", component_property="value"),
+    State(component_id="input-handle", component_property="value"),
+)
+def display_value(nclicks, num, acnt_handle):
+    results = api.GetSearch(
+        raw_query=f"q=%40{acnt_handle}&src=typed_query&count={num}"
+    )       #       q=%40MoveTheWorld%20until%3A2021-08-05%20since%3A2021-01-01&src=typed_query
+
+    twt_followers, twt_likes, twt_count, twt_friends, twt_name = [], [], [], [], []
+    for line in results:
+        twt_likes.append(line.user.favourites_count)
+        twt_followers.append(line.user.followers_count)
+        twt_count.append(line.user.statuses_count)
+        twt_friends.append(line.user.friends_count)
+        twt_name.append(line.user.screen_name)
+
+        print(line)
+
+    d = {
+        "followers": twt_followers,
+        "likes": twt_likes,
+        "tweets": twt_count,
+        "friends": twt_friends,
+        "name": twt_name,
+    }
+    df = pd.DataFrame(d)
+    print(df.head())
+
+    most_followers = df.followers.max()
+    most_folwrs_account_name = df["name"][df.followers == most_followers].values[0]
+
+    scatter_fig = px.scatter(
+        df, x="followers", y="likes", trendline="ols", hover_data={"name": True}
+    )
+    scatter_fig2 = px.scatter(
+        df, x="friends", y="likes", trendline="ols", hover_data={"name": True}
+    )
+    message = f"La cuenta de Twitter @{most_folwrs_account_name} menciona #{acnt_handle} tiene el mayor número de seguidores: ( {most_followers} )."
+
+    return scatter_fig, scatter_fig2, message
